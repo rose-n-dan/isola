f47b3c4209ab54dc76bab337b07c5fc21e525d64
Michal Rosa <rosjan.rosa@gmail.com>
2019-03-27 01:42:21 +0100

    Added MenuScreens
    Added Menu Screens
    Remove Checkers drawing (invalid drawing)

--- scripts/GUI.py
+++ scripts/GUI.py
@@ -3,25 +3,40 @@ from kivy.app import App
 from kivy.clock import Clock
 from kivy.vector import Vector
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+import kivy.graphics
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 from scripts.Board import Board, Cell
 
 
+class MainWindow(Screen):
+    pass
+
+
+class OptionWindow(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
 class Checker(Widget):
-    obj = Board()
+    game_board = Board()
 
     def move(self):
         for i in range(7):
             for j in range(7):
-                if self.obj.board[i, j] == Cell.PLAYER_WHITE:
+                if self.game_board.board[i, j] == 'W':
                     self.pos = Vector(250*j, 100*i+30)
-                elif self.obj.board[i, j] == Cell.PLAYER_BLACK:
-                    self.pos = Vector(20*j, 100*i)
+                elif self.game_board.board[i, j] == 'B':
+                    self.pos = Vector(20*j, 200*i)
 
 
-
-
-class IsolaGame(Widget):
+class IsolaGame(Screen):
+    board = Board()
     checkerB = kivy.properties.ObjectProperty(None)
     checkerW = kivy.properties.ObjectProperty(None)
 
@@ -33,14 +48,24 @@ class IsolaGame(Widget):
         self.checkerB.move()
         self.checkerW.move()
 
+    def on_touch_move(self, touch):
+        if self.checkerW.pos == touch.pos:
+            self.checkerW.pos = touch.pos
+        if self.checkerB.pos == touch.pos:
+            self.checkerB.pos = touch.pos
+
 
 class IsolaApp(App):
     def build(self):
+        return kv
+
         game = IsolaGame()
         game.start_game()
         Clock.schedule_interval(game.update, 1.0 / 60.0)
         return game
 
+
+kv = Builder.load_file("Isola.kv")
 
 if __name__ == '__main__':
     IsolaApp().run()
